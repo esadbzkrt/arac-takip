53553b9c11105f89da1cafaea1ff894119df201d
esadbzkrt <esadbzkrt@gmail.com>
2022-09-25 19:48:55 +0300

    login page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import './Header.scss';
 
 import React from 'react';
-import {BiCar}  from "react-icons/bi";
+import {BiCar} from "react-icons/bi";
 
 const Header = () => {
     return (
         <div className="header">
-            <div className="header-logo">
-                <BiCar className="header-logo-icon"/>
-                <h1 className="header-logo-title">Vehicle Task</h1>
+            <div className="header-items">
+                <BiCar className="header-items-icon"/>
+                <h1 className="header-items-title">Vehicles</h1>
+                <h1 className="header-items-title">Models</h1>
+                <h1 className="header-items-title">Locations</h1>
+                <div className="header-items-space"></div>
+                <button>Logout</button>
             </div>
-
         </div>
     );
 };
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,13 +1,19 @@
 import './Login.scss';
-
-
 import React from 'react';
 
 const Login = () => {
+
     return (
-        <div>
-            Login Page
-        </div>
+            <div className="login">
+                <h1 className="login-title">Vehicle Tracking Login Page</h1>
+                <div className="login-input">
+                    <h3 className="login-input-title">Username</h3>
+                    <input className="username" type="text"/>
+                    <h3 className="login-input-title">Password</h3>
+                    <input type="password" />
+                </div>
+                <button className="login-button">Login</button>
+            </div>
     );
 };
 
